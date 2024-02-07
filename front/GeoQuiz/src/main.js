e70ddfa9fb7fc923c532a86633f494680d7a7e95
oudin-clement <clementoudin3@gmail.com>
2024-02-07 16:57:38 +0100

    api plugin

--- front/GeoQuiz/src/main.js
+++ front/GeoQuiz/src/main.js
@@ -5,9 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import apidirectus from "@/plugin/api.js";
+import apigeolo from "@/plugin/api.js";
+import apiauth from "@/plugin/api.js";
 
 const app = createApp(App)
 
+
+app.use(apidirectus, { baseURL: 'http://docketu.iutnc.univ-lorraine.fr:37206'})
+app.use(apigeolo, { baseURL: 'http://docketu.iutnc.univ-lorraine.fr:37202'})
+app.use(apiauth, { baseURL: 'http://docketu.iutnc.univ-lorraine.fr:37201'})
 app.use(createPinia())
 app.use(router)
 
