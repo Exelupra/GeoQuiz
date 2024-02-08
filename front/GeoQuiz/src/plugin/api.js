f61432da12901082899e787dd3cd0bc5262f669c
oudin-clement <clementoudin3@gmail.com>
2024-02-08 09:48:30 +0100

    api plugin correction

--- front/GeoQuiz/src/plugin/api.js
+++ front/GeoQuiz/src/plugin/api.js
@@ -2,11 +2,26 @@ import axios from 'axios'
 
 export default {
     install: (app, options) => {
-        app.config.globalProperties.$api = axios.create({
-            baseURL: options.baseURL,
+        app.config.globalProperties.$apidirectus = axios.create({
+            baseURL: 'http://docketu.iutnc.univ-lorraine.fr:37206',
             headers: {
+                "Access-Control-Allow-Origin": "*",
                 "Content-type": "application/json"
             }
-        })
+        });
+        app.config.globalProperties.$apigeolo = axios.create({
+            baseURL: 'http://docketu.iutnc.univ-lorraine.fr:37202',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-type": "application/json"
+            }
+        });
+        app.config.globalProperties.$apiauth = axios.create({
+            baseURL: 'http://docketu.iutnc.univ-lorraine.fr:37201',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-type": "application/json"
+            }
+        });
     }
 }
